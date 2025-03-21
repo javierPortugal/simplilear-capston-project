113647b575c7fde171d99869e434cd7506789a04
Javier Portugal <javier.s.portugal@gmail.com>
2025-03-21 08:02:05 -0600

    update
    update form

--- Frontend/Frontend/src/components/userform.jsx
+++ Frontend/Frontend/src/components/userform.jsx
@@ -1,3 +1,4 @@
+/*
 import axios from 'axios';
 import { useState } from 'react';
 
@@ -7,7 +8,7 @@ function UserForm({fetchData}){
 
     const handleSubmit=async(e)=>{
         e.preventDefault();
-        const resp=await axios.post('http://localhost:/api/user/', user);
+        const resp=await axios.post('http://localhost:5000/api/user/', user);
         console.log(resp);
 
         if (resp.status==201){
@@ -51,6 +52,57 @@ function UserForm({fetchData}){
           </form>  
 
         </div>
+    );
+}
+
+export default UserForm;
+*/
+import axios from 'axios';
+import { useState } from 'react';
+
+
+function UserForm({fetchData}){
+    const [user,setUser]=useState({name:'',email:'',username:'',password:''});
+    const handleSubmit=async(e)=>{
+        e.preventDefault();
+        const resp=await axios.post('http://localhost:5000/api/user/',user);
+        console.log(resp);
+        if(resp.status==201){
+            alert('User Registered Successfully');
+            fetchData();
+            setUser({name:'',email:'',username:'',password:''});
+        }else{
+            alert('Error in Registration');
+        }
+    }
+    return(<div>
+        <form onSubmit={handleSubmit}>
+            <div className='mb-3'>
+                <label>Name</label>
+                <input type='text' placeholder='Enter Your Name' className='form-control'
+                value={user.name} onChange={(e)=> setUser({...user,name:e.target.value})}></input>
+            </div>
+            <div className='mb-3'>
+                <label>UserName</label>
+                <input type='text' placeholder='Enter Your Username' className='form-control'
+                value={user.username} onChange={(e)=> setUser({...user,username:e.target.value})}></input>
+            </div>
+
+            <div className='mb-3'>
+                <label>Email</label>
+                <input type='text' placeholder='Enter Your Username' className='form-control'
+                value={user.email} onChange={(e)=> setUser({...user,email:e.target.value})}></input>
+            </div>
+
+            <div className='mb-3'>
+                <label>Password</label>
+                <input type='text' placeholder='Enter Your Username' className='form-control'
+                value={user.password} onChange={(e)=> setUser({...user,password:e.target.value})}></input>
+            </div>
+            <button type='submit' className='btn btn-primary mt-3'>Register</button>
+            
+        </form>
+    </div>
     );
 }
 
--- Frontend/Frontend/src/main.jsx
+++ Frontend/Frontend/src/main.jsx
@@ -5,8 +5,9 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.js'
 import App from './App.jsx'
 
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  //<StrictMode>
     <App />
-  </StrictMode>,
+  //</StrictMode>,
 )
